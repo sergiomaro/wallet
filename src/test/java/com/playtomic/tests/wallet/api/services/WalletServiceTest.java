87ff8d8cb3c5e4832ca51fa0a4bd6d65c2f1e4ff
seghis <sergiomr@usal.es>
2021-08-28 15:56:48 +0200

    test: added to wallet service

---
+++ src/test/java/com/playtomic/tests/wallet/api/services/WalletServiceTest.java
@@ -0,0 +1,43 @@
+package com.playtomic.tests.wallet.api.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.playtomic.tests.wallet.api.entities.Wallet;
+import com.playtomic.tests.wallet.api.repositories.WalletRepository;
+
+@SpringBootTest
+public class WalletServiceTest {
+
+	@Autowired
+	private WalletService walletService;
+
+	@MockBean
+	private WalletRepository walletRepository;
+
+	@Test
+	public void getWalletById_ok() throws WalletServiceException {
+		Wallet wallet = new Wallet();
+		wallet.setId(1);
+		wallet.setAmount(new BigDecimal("10.00"));
+
+		Mockito.when(walletRepository.findById(wallet.getId())).thenReturn(Optional.of(wallet));
+
+		assertThat(walletService.getWalletById(1)).isEqualTo(wallet);
+	}
+
+	@Test
+	public void getWalletById_error() throws WalletServiceException {
+		assertThrows(WalletServiceException.class, () -> walletService.getWalletById(1));
+	}
+
+}
